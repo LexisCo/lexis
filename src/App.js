3d2f55197bf19e046b5a92fa1f3f0afcc72bd842
Anthony Tam <anthonyy.tam@gmail.com>
2018-04-22 02:40:03 -0400

    Test modify App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-test">
+        	Hello this is Anthony, testing deployment + commits.
+        </p>
       </div>
     );
   }
